c4700fc79525087ea188ec2d1ab909a2479c9fed
rejectflamingo <Julianruiz737@gmail.com>
2021-04-11 01:05:10 -0500

    Poggies, changed settings

--- DjangoFrontend/zugzugsite/julian/models.py
+++ DjangoFrontend/zugzugsite/julian/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+    genre_id = models.IntegerField()
+    genre_name = models.CharField(max_length=30)
--- DjangoFrontend/zugzugsite/zugzugsite/settings.py
+++ DjangoFrontend/zugzugsite/zugzugsite/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'julian',
+    'kevin',
+    'jacob',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
